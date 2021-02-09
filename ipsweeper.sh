864a561019b02a2969128ba4ab43f086a3870c98
Isaacam7 <68027601+Isaacam7@users.noreply.github.com>
2021-02-09 12:14:57 -0500

    Create ipsweeper.sh

---
+++ ipsweeper.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if ["$1" == ""]
+then
+        echo "incorrect syntax"
+        echo "include first three fields of ip address"
+else
+        for i in {1..254}
+                do
+                        ping -c 1 $1.$i | grep 64 | cut -d ' ' -f 4 | cut -d ':>
+                done
+fi
+
+
